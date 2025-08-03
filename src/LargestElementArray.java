5b9fd2fec3b1a38ce48c596a2a144494c9d27bc8
[pushkar patil] <[pushkarp144@gmail.com]>
2025-08-04 02:46:59 +0530

    .\src\

--- src/LargestElementArray.java
+++ src/LargestElementArray.java
@@ -19,7 +19,7 @@ static class Solution {
 
 public static void main(String[] args) {
     Solution sol = new Solution();
-    int[] nums = {4,9,3,5,5,2,9,7};
+    int[] nums = {4,9,3,5,2,9,7};
     int result = sol.largestElement(nums);
     System.out.println("The largest element is: " + result);
     
